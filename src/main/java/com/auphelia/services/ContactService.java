ab70adbc50ff1e9f1cff7b4e9e7f17bbacdabf28
François Fortin <ff@fftstudios.com>
2013-05-07 08:16:10 -0400

    Added header

--- src/main/java/com/auphelia/services/ContactService.java
+++ src/main/java/com/auphelia/services/ContactService.java
@@ -3,6 +3,8 @@ package com.auphelia.services;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import com.auphelia.models.Contact;
 
@@ -11,7 +13,11 @@ public class ContactService {
 
 	@GET()
 	@Produces("application/json; charset=UTF-8")
-	public Contact sayHello() {
-		return new Contact("Massi", "AZAMOUM", "rue jean-talon", "Montréal", "QC", "H2L 1X7");
+	public Response sayHello() {
+		Contact c = new Contact("Massi", "AZAMOUM", "rue jean-talon", "Montréal", "QC", "H2L 1X7");
+		
+		ResponseBuilder builder = Response.ok(c);
+		builder.language("fr").header("Access-Control-Allow-Origin", "*");
+		return builder.build();
 	}
 }
