75f16c47d302db2bebe6df67a509bde43b545e13
François Fortin <ff@fftstudios.com>
2013-05-07 10:31:39 -0400

    Added CORS library

--- src/main/java/com/auphelia/services/ContactService.java
+++ src/main/java/com/auphelia/services/ContactService.java
@@ -3,8 +3,6 @@ package com.auphelia.services;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
 import com.auphelia.models.Contact;
 
@@ -13,11 +11,9 @@ public class ContactService {
 
 	@GET()
 	@Produces("application/json; charset=UTF-8")
-	public Response sayHello() {
+	public Contact sayHello() {
 		Contact c = new Contact("Massi", "AZAMOUM", "rue jean-talon", "Montréal", "QC", "H2L 1X7");
 		
-		ResponseBuilder builder = Response.ok(c);
-		builder.language("fr").header("Access-Control-Allow-Origin", "*");
-		return builder.build();
+		return c;
 	}
 }
